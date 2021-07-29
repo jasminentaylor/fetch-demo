f0fd89e5697575fef6d7f1d897f203950bb94db2
Jasmine Taylor <jasminentaylor91@gmail.com>
2021-07-29 16:05:03 -0400

    fetch request change appended image and button works

--- main.js
+++ main.js
@@ -4,12 +4,20 @@ const countryDiv = document.querySelector('.country-div')
 
 // add event listener to our button
 button.addEventListener('click', function() {
+    //Grab what the user types
+    const country = input.value;
     // Fetch our data
-    fetch('https://restcountries.eu/rest/v2/name/usa')
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then((response) => response.json())
     .then((data) => {
         const countryData = data[0];
         console.log(countryData);
+        // array destructuring 
         //const [countryData] = data;
+
+        const image = document.createElement('img');
+        image.src = countryData.flag;
+        image.classList.add('flag')
+        document.body.append(image);
     });
 });
