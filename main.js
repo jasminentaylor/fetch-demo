634d92512841822b899b0ff7cef01191248bb78b
Jasmine Taylor <jasminentaylor91@gmail.com>
2021-07-29 15:41:02 -0400

    set up our event listener and fetch info

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const input = document.querySelector('input');
+const button = document.querySelector('button')
+const countryDiv = document.querySelector('.country-div')
+
+// add event listener to our button
+button.addEventListener('click', function() {
+    // Fetch our data
+    fetch('https://restcountries.eu/rest/v2/name/usa')
+    .then((response) => response.json())
+    .then((data) => {
+        const countryData = data[0];
+        console.log(countryData);
+        //const [countryData] = data;
+    });
+});
